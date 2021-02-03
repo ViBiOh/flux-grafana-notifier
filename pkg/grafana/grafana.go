5bd0a868c980e1ef4d185895ae664c74daafd864
Vincent Boutour <bob@vibioh.fr>
2021-02-03 18:14:19 +0100

    fix: Changing order of HasPrefix

--- pkg/grafana/grafana.go
+++ pkg/grafana/grafana.go
@@ -82,7 +82,7 @@ func (a app) Handler() http.Handler {
 }
 
 func (a app) send(ctx context.Context, text string, tags ...string) {
-	if strings.HasPrefix("no update", text) || len(text) > 255 {
+	if strings.HasPrefix(text, "no update") || len(text) > 255 {
 		return
 	}
 
