5b0c793fe0056469234635cb99343da7d4010547
Vincent Boutour <bob@vibioh.fr>
2023-01-06 22:36:21 +0100

    chore(deps): Bumping deps and breaking changes
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- cmd/notifier/api.go
+++ cmd/notifier/api.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"net/http"
@@ -53,9 +54,11 @@ func main() {
 	logger.Global(logger.New(loggerConfig))
 	defer logger.Close()
 
-	tracerApp, err := tracer.New(tracerConfig)
+	ctx := context.Background()
+
+	tracerApp, err := tracer.New(ctx, tracerConfig)
 	logger.Fatal(err)
-	defer tracerApp.Close()
+	defer tracerApp.Close(ctx)
 	request.AddTracerToDefaultClient(tracerApp.GetProvider())
 
 	go func() {
@@ -82,8 +85,8 @@ func main() {
 		httperror.NotFound(w)
 	})
 
-	go promServer.Start("prometheus", healthApp.End(), prometheusApp.Handler())
-	go appServer.Start("http", healthApp.End(), httputils.Handler(handler, healthApp, recoverer.Middleware, prometheusApp.Middleware, tracerApp.Middleware, owasp.New(owaspConfig).Middleware, cors.New(corsConfig).Middleware))
+	go promServer.Start(healthApp.ContextEnd(), "prometheus", prometheusApp.Handler())
+	go appServer.Start(healthApp.ContextEnd(), "http", httputils.Handler(handler, healthApp, recoverer.Middleware, prometheusApp.Middleware, tracerApp.Middleware, owasp.New(owaspConfig).Middleware, cors.New(corsConfig).Middleware))
 
 	healthApp.WaitForTermination(appServer.Done())
 	server.GracefulWait(appServer.Done(), promServer.Done())
