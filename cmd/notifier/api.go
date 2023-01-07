ebd20f8f18d2705539c269037ed1160c7efb40e1
Vincent Boutour <bob@vibioh.fr>
2023-01-07 10:26:48 +0100

    chore(deps): Bumping httputils and reducing number of goroutine
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- cmd/notifier/api.go
+++ cmd/notifier/api.go
@@ -85,8 +85,10 @@ func main() {
 		httperror.NotFound(w)
 	})
 
-	go promServer.Start(healthApp.ContextEnd(), "prometheus", prometheusApp.Handler())
-	go appServer.Start(healthApp.ContextEnd(), "http", httputils.Handler(handler, healthApp, recoverer.Middleware, prometheusApp.Middleware, tracerApp.Middleware, owasp.New(owaspConfig).Middleware, cors.New(corsConfig).Middleware))
+	endCtx := healthApp.End(ctx)
+
+	go promServer.Start(endCtx, "prometheus", prometheusApp.Handler())
+	go appServer.Start(endCtx, "http", httputils.Handler(handler, healthApp, recoverer.Middleware, prometheusApp.Middleware, tracerApp.Middleware, owasp.New(owaspConfig).Middleware, cors.New(corsConfig).Middleware))
 
 	healthApp.WaitForTermination(appServer.Done())
 	server.GracefulWait(appServer.Done(), promServer.Done())
